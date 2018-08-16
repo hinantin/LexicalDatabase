dc5d112e314934978bc43f3ef042331f83173257
rcastromamani <richard.castro.mamani@gmail.com>
2018-08-16 16:35:31 -0500

    Adding more content

--- ADBL.sql
+++ ADBL.sql
@@ -10,3 +10,18 @@ CREATE TABLE `Lexical_Unit`
 `Is_OWLU` BIT(1), 
 PRIMARY KEY (`Id_Lexical_Unit`)
 );
+
+CREATE TABLE `Spelling`
+(
+`Id_Spelling` VARCHAR(10) NOT NULL, 
+`Word` BIT(1), 
+`Spelling_System` BIT(1), 
+`Source` BIT(1), 
+`Definition` BIT(1), 
+`Gloss` BIT(1), 
+`Definition_Sp` BIT(1), 
+`Gloss_Sp` BIT(1), 
+`Example` BIT(1), 
+`Frequency` BIT(1), 
+PRIMARY KEY (`Id_Lexical_Unit`)
+);
