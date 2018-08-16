69b6af4de475919e21c849e980d974d11130a5a9
rcastromamani <richard.castro.mamani@gmail.com>
2018-08-16 15:28:09 -0500

    Adding more content

--- ADBL.sql
+++ ADBL.sql
@@ -5,8 +5,8 @@ DROP TABLE IF EXISTS `Lexical_Unit`;
 CREATE TABLE `Lexical_Unit`
 (
 `Id_Lexical_Unit` VARCHAR(10) NOT NULL, 
-`Is_Loan_Word` VARCHAR(10), 
-`Is_Dictionary_Entry` VARCHAR(10), 
-`Is_OWLU` VARCHAR(20), 
+`Is_Loan_Word` BIT(1), 
+`Is_Dictionary_Entry` BIT(1), 
+`Is_OWLU` BIT(1), 
 PRIMARY KEY (`Id_Lexical_Unit`)
 );
