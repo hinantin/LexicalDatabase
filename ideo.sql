95c934f02947318ade1aff2fafa3344eb0cdd66b
rcastromamani <richard.castro.mamani@gmail.com>
2018-08-19 18:07:17 -0500

    Adding more vocab.

--- ideo.sql
+++ ideo.sql
@@ -1,3 +1,16 @@
 USE `ADBL`;
 
-SELECT CONCAT('| \"[=',`Word`,'', IFNULL(`Morphological_Values`,""), '][Ideo][=',`Definition_EN`,']\" : {',`Word`,'}') FROM `Spelling`;
+-- SELECT CONCAT('| \"[=',`Word`,'', IFNULL(`Morphological_Values`,''), '][Ideo][=',`Definition_EN`,'',,']\" : {',`Word`,'}') AS `Entry` FROM `Spelling`;
+
+DROP PROCEDURE IF EXISTS `sp_QueryEntries`;
+
+DELIMITER //
+CREATE PROCEDURE `sp_QueryEntries`
+(
+	IN IN_WordClass VARCHAR(30) 
+)
+BEGIN 
+  SELECT CONCAT('| \"[=',`Word`,'', IFNULL(`Morphological_Values`,''), '][',IN_WordClass,'][=',`Definition_EN`,'',,']\" : {',`Word`,'}') AS `Entry` FROM `Spelling`;
+END //
+
+DELIMITER ;
