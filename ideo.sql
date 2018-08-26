e945519ec5917d3cca1cba70e065cd733891c5d4
rcastromamani <richard.castro.mamani@gmail.com>
2018-08-26 11:22:32 -0500

    Adding content.

--- ideo.sql
+++ ideo.sql
@@ -10,7 +10,7 @@ CREATE PROCEDURE `sp_QueryEntries`
 	IN IN_WordClass VARCHAR(30) 
 )
 BEGIN 
-  SELECT CONCAT('| \"[=',`Word`,'', IFNULL(`Morphological_Values`,''), '][',IN_WordClass,'][=',`Definition_EN`,'',,']\" : {',`Word`,'}') AS `Entry` FROM `Spelling`;
+  SELECT CONCAT('| \"[=',`Word`,'', IFNULL(`Morphological_Values`,''), '][',IN_WordClass,'][=',`Definition_EN`,'',IF(ISNULL(`Definition_ES`),'',CONCAT(' (ES: ',`Definition_ES`,')')),']\" : {',`Word`,'}', IF(ISNULL(`Source`),'',CONCAT(' ! ',`Source`))) AS `Entry` FROM `Spelling`;
 END //
 
 DELIMITER ;
